245868979a87189f483be7028d665772c80c828b
Abhay Chabuk <chabukabhay@gmail.com>
2025-03-06 15:19:38 +0000

    Got to know the syntax by which we can make queue.

--- Queue/Main.java
+++ Queue/Main.java
@@ -8,3 +8,28 @@
 
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Queue<String> queue = new LinkedList<String>();
+
+        queue.offer("Abhay");
+        queue.offer("Bhagwan");
+        queue.offer("Chabuk");
+        
+        queue.poll();
+        queue.poll();
+        queue.poll();
+
+        System.out.println(queue);
+        // System.out.println(queue.peek());
+
+        System.out.println(queue.isEmpty());
+
+        System.out.println(queue.contains("Abhay"));
+        
+    }
+}
