44b4c09f2d4652a11d175fa8a0c73fabb4561164
Abhay Chabuk <chabukabhay@gmail.com>
2025-03-06 15:05:33 +0000

    Got to know about Queue. Its basic info

---
+++ Queue/Main.java
@@ -0,0 +1,10 @@
+// Queue = FIFO data structure. First in First out
+//         A collection designed for holding elements prior to processing
+//         Linear data structure
+
+
+//         add      =  enqueue, offer()
+//         remove   =  dequeue, poll()
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
